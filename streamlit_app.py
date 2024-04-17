267895f38bb9ba6a9c744f12e4c6035b59951cbb
divyapala92 <166137266+divyapala92@users.noreply.github.com>
2024-04-17 17:25:05 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,9 @@
 import streamlit as st
 import pandas as pd
-import joblib
-
-# Import performance metrics and confusion matrix from the training script
-from training_script import performance_metrics, cm
+from training_script import rf, cm
 
 # Streamlit UI
 st.title('Diabetes Checkup')
-
-# Display model performance metrics
-st.write("## Model Performance Metrics")
-st.write(f"Accuracy: {performance_metrics['accuracy'] * 100:.2f}%")
-st.write(f"Precision: {performance_metrics['precision']:.2f}")
-st.write(f"Recall: {performance_metrics['recall']:.2f}")
-st.write(f"F1 Score: {performance_metrics['f1']:.2f}")
 
 # Displaying the confusion matrix and performance metrics
 st.write("## Confusion Matrix and Performance Metrics")
