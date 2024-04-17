b315b44d13740b9bb98d1b95635bc762aafda926
divyapala92 <166137266+divyapala92@users.noreply.github.com>
2024-04-17 16:08:37 +1000

    Update training_script.py

--- training_script.py
+++ training_script.py
@@ -23,6 +23,20 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Initialise and train a random forest classifier using training data
 rf = RandomForestClassifier()
 rf.fit(x_train, y_train)
+# Use trained model to make predicionst on the test set
+y_pred = rf.predict(x_test)
+
+# Calculate the confusion matrix and performance metrics
+cm = confusion_matrix(y_test, y_pred)
+TP = cm[1, 1]
+TN = cm[0, 0]
+FP = cm[0, 1]
+FN = cm[1, 0]
+
+accuracy = (TP + TN) / (TP + TN + FP + FN)
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+f1 = 2 * (precision * recall) / (precision + recall)
 
 # Save the trained model to a file
 joblib.dump(rf, "trained_model.pkl")
