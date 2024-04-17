900fa0aaa3fa4f89135e1b3430fcb9e28f78d785
divyapala92 <166137266+divyapala92@users.noreply.github.com>
2024-04-17 16:28:32 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,29 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 # Load the trained model
 loaded_model = joblib.load("trained_model.pkl")
 
+# Load the label encoder
+label_encoder = joblib.load("label_encoder.pkl")
+
 # Load the test data
 test_data = pd.read_csv("Clean_BDHS_Diabetic_Data_Jahan_Balanced.csv")
 X_test = test_data[['weight', 'height', 'SBP', 'DBP', 'age']]
 y_test = test_data['diabetes']
 
+# Apply label encoding to y_test
+y_test_encoded = label_encoder.transform(y_test)
+
 # Make predictions on the test data
 y_pred = loaded_model.predict(X_test)
 
 # Ensure both y_test and y_pred are one-dimensional arrays
-y_test = y_test.values.ravel()
+y_test_encoded = y_test_encoded.ravel()
 y_pred = y_pred.ravel()
 
 # Calculate performance metrics
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
+accuracy = accuracy_score(y_test_encoded, y_pred)
+precision = precision_score(y_test_encoded, y_pred)
+recall = recall_score(y_test_encoded, y_pred)
+f1 = f1_score(y_test_encoded, y_pred)
 
 # Display performance metrics
 st.write("## Model Performance Metrics")
@@ -34,4 +40,4 @@ st.write(f"F1 Score: {f1:.2f}")
 # Displaying the confusion matrix
 st.write("## Confusion Matrix")
 st.write("Confusion Matrix:")
-st.write(confusion_matrix(y_test, y_pred))
+st.write(confusion_matrix(y_test_encoded, y_pred))
